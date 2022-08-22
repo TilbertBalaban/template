07f384dbaabee3c89814fca85573185bbd40a99a
Tilbert Balaban <tilbert.balaban@gmail.com>
2022-08-22 11:39:36 +0300

    feat: decomposed vehicles and starships code

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,12 @@
 import './App.css';
-import fetchStarships from './redux/actions/fetchStarships';
-import { useTypedDispatch, useTypedSelector } from './redux/hooks';
+import { StarshipsContainer } from './views/containers/Starships';
+import { VehiclesContainer } from './views/containers/Vehicles';
 
-function App() {
-  const { starships, error } = useTypedSelector((state) => state.starshipsRoot);
-
-  const dispatch = useTypedDispatch();
-  console.log(starships);
-  return (
-    <div className="App">
-      {error && <div>{error}</div>}
-      {starships.length
-        ? (
-          <div>
-            { starships.map((starship) => (
-              <div>
-                {starship.name}
-              </div>
-            ))}
-          </div>
-        )
-        : 'load'}
-      <button
-        onClick={() => {
-          dispatch(fetchStarships());
-        }}
-        type="button"
-      >
-        load ships
-      </button>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <VehiclesContainer />
+    <StarshipsContainer />
+  </div>
+);
 
 export default App;

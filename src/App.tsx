423f4793009cdf02cf3d13ba83efc5e6727b7465
Tilbert Balaban <tilbert.balaban@gmail.com>
2022-10-04 15:55:45 +0300

    feat: add mouse-spy frog component

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { ItemsContainer } from './views/containers/Items';
 import { VehiclesContainer } from './views/containers/Vehicles';
 import { GraphQlAndApolloExample } from './views/containers/GraphQlAndApolloExample';
 import { HeaderContainer } from './views/containers/Header';
+import { SpyingFrog } from './views/containers/SpyingFrog/SpyingFrog';
 
 const App = () => (
   <div data-testid="home">
     <HeaderContainer />
+    <SpyingFrog />
     <VehiclesContainer />
     <ItemsContainer />
     <PostsContainer />
